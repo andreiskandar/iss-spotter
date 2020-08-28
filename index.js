8d4426f195ef59460554494870e44b835a7b87d8
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-27 19:42:14 -0700

    completed spotter 4

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 const { nextISSTimesForMyLocation } = require('./iss');
-const moment = require('moment-timezone');
 
-nextISSTimesForMyLocation(callback => {
-	if (error) {
+/*
+nextISSTimesForMyLocation(passTimes => {
+	// if (error) {
+	// 	return console.log("It didn't work! ", error);
+	// }
+
+	// fetchMyIP((error, ip) => {
+	// 	if (error) {
+	// 		console.log("It didn't work! ", error);
+	// 		return;
+	// 	}
+	// 	console.log('It worked! Returned IP: ', ip);
+
+	// 	fetchCoordsByIP(ip.ip, (error, coords) => {
+	// 		if (error) {
+	// 			console.log('Error: ', error);
+	// 			return;
+	// 		}
+	// 		console.log('My coordinates: ', coords);
+
+	// 		fetchISSFlyOverTimes(coords, (err, passes) => {
+	// 			if (err) {
+	// 				console.log('Error: ', error);
+	// 				return;
+	// 			}
+	// 			passTimes(passes);
+
+	// 			console.log('It worked! Returned flyover times: ', passes);
+	// 		});
+	// 	});
+	// });
+});
+*/
+
+const printPassTimes = passTimes => {
+	passTimes.forEach(item => {
+		const date = new Date(item.risetime * 1000);
+		const duration = item.duration;
+		console.log(`Next pass at ${date} for ${duration} seconds`);
+	});
+};
+
+nextISSTimesForMyLocation((err, passTimes) => {
+	if (err) {
 		return console.log("It didn't work! ", error);
 	}
-	fetchMyIP((error, ip) => {
-		if (error) {
-			console.log("It didn't work! ", error);
-			return;
-		}
-		console.log('It worked! Returned IP: ', ip);
-
-		fetchCoordsByIP(ip.ip, (error, coords) => {
-			if (error) {
-				console.log('Error: ', error);
-				return;
-			}
-			console.log('My coordinates: ', coords);
-
-			fetchISSFlyOverTimes(coords, (err, passes) => {
-				if (err) {
-					console.log('Error: ', error);
-					return;
-				}
-				console.log('It worked! Returned flyover times: ', passes);
-			});
-		});
-	});
+	printPassTimes(passTimes);
 });
-
-// var s = new Date(1504095567183).toLocaleDateString("en-US")
-// console.log(s)
-// // expected output "8/30/2017"
-// and for time:
-
-// var s = new Date(1504095567183).toLocaleTimeString("en-US")
-// console.log(s)
-// // expected output "3:19:27 PM"
